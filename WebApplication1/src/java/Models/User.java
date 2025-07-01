a6a84675bfd3c57b7ea32f2403a4d8917dd7c98c
hnzgf <hung874864@gmail.com>
2025-07-01 21:34:13 +0700

    Models package added

---
+++ WebApplication1/src/java/Models/User.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package Models;
+
+
+public class User {
+private int Id;
+private String Name;
+private int Age;
+private boolean Gender;
+
+    public User() {
+    }
+
+    public User(int Id, String Name, int Age, boolean Gender) {
+        this.Id = Id;
+        this.Name = Name;
+        this.Age = Age;
+        this.Gender = Gender;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int Age) {
+        this.Age = Age;
+    }
+
+    public boolean isGender() {
+        return Gender;
+    }
+
+    public void setGender(boolean Gender) {
+        this.Gender = Gender;
+    }
+
+}
