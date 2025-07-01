679ca39f3a875a957d55fc93690cfa5b00954118
khanhs2105 <nguyendinhkhanh2k5@gmail.com>
2025-07-01 21:45:13 +0700

    sua DAO

--- WebApplication1/src/java/DAL/DAO.java
+++ WebApplication1/src/java/DAL/DAO.java
@@ -5,7 +5,24 @@
 
 package DAL;
 
+import Models.User;
+import java.sql.Connection;
+import java.util.Vector;
+
 
 public class DAO {
+    
+    private String status = "ok";
+    private Connection con;
+    private Vector<User> std;
+  
 
+    public static final DAO Ins = new DAO();
+        public DAO() {
+        try {
+            con = new DBContext().getConnection();
+        } catch (Exception e) {
+            status = "Error at connecttion" + e.getMessage();
+        }
+    }
 }
